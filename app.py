5fafc116d647d875fc2b9001d48c0af0ab4384d4
carlogovich <cgongoraph@gmail.com>
2026-09-06 08:34:34 -0500

    app.py

--- app.py
+++ app.py
@@ -362,7 +362,7 @@ def crear_sparkline(
                 size=12,
                 color="#9ba8b5"
             ),
-            tickangle=30
+            tickangle=-30
         ),
 
         yaxis=dict(
